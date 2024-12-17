90ed8079d70b57981e1bb00f50b0a5675008e940
ali-raza-mukhtiar <muhammadalirazamukhtiar@gmail.com>
2024-12-17 14:38:10 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,77 @@
+// Chapter No 1 Alerts(ali raza)
+
+// Question No 1
+// alert("wellcome to javascript");
+
+//Question No 2
+// alert("Error! Please enter valid password to continue.");
+
+//Question No 3
+// alert("Welcome to JS Land \n Happy Coding");
+
+//Question No 4
+//alert("Welcome to JS Land");
+//alert("Happy Coding");
+
+//Question No 5
+//console.log("Hello..I can run JS through my web browsers console");
+
+
+
+//Chapter No 2 Variables for Strings
+
+//Question No 1
+
+// var myName= ("Ali raza");
+// var nationality;
+
+// var message= ("Hello World");
+// alert(message);
+
+// var bioData= ("Certified Mobile Application Development");
+// alert(bioData);
+
+//Question No 2
+
+// var foodName=("Pizza\nPizz\nPiz\nPi\nP");
+// alert(foodName);
+
+//Question No 3
+
+//var email=("example@gmail.com");
+//alert(email);
+
+//Question No 4
+
+//var book=("A smarter way to learn Java Script");
+//alert(book);
+
+//Question No 5
+
+// document.write("<h1>I am trying to learn from “A smarter way to learn JavaScript”..</h1>")
+
+// Chapter No 3 Variables for Numbers
+
+//Question No 1
+
+//var age=(17);
+//alert(age);
+
+//Question No 2
+
+// var visitors=("You have visited this site 4 times.");
+// alert(visitors);
+
+//Question No 3
+
+// var birthYear="2007"
+// document.write("<h4>" + birthYear + "</h4>");
+
+//Question No 4
+
+// var customerName = (" Mr muzammil ordered \n");
+// var quantity = (5);
+// var product = ("T-shirt(s) on \n");
+// var store = ("ali raza Brand")
+
+// document.write(customerName + quantity + product + store)
